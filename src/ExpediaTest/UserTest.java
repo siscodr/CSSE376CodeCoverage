bf2640633a46676a79d917c7a6ce354469eab474
siscodr <siscodr@rose-hulman.edu>
2015-04-17 19:11:27 -0400

    Updated User Tests
    Found a bug!

--- src/ExpediaTest/UserTest.java
+++ src/ExpediaTest/UserTest.java
@@ -32,6 +32,15 @@ public class UserTest
 	}
 	
 	@Test
+	public void TestThatDiscountInitializes()
+	{
+	Discount target = new Discount(0.01, 1);
+	ServiceLocator.Instance().AddDiscount(target);
+	this.target.book(new Booking[]{new Flight(StartDate, EndDate, 100), new Hotel(5), new Car(3)});
+	assertEquals(1024.65,this.target.Price(), 0.01);
+	}
+
+	@Test
 	public void TestThatUserHasZeroFrequentFlierMilesOnInit()
 	{
 		Assert.assertEquals(0, target.FrequentFlierMiles());
@@ -97,9 +106,17 @@ public class UserTest
 		Car car = new Car(5);
 		Flight flight = new Flight(StartDate, EndDate, 10000);
 		target.bookWithDoubleMiles(new Booking[]{flight});
-		Assert.assertEquals(5000, target.bonusFrequentFlierMiles, 0.01);
+		Assert.assertEquals(5000, target.bonusFrequentFlierMiles, 0.01);	
 	}
-
+	
+	@Test
+	public void TestDoubleMilesLessThan5000()
+	{
+		Car car = new Car(5);
+		Flight flight = new Flight(StartDate, EndDate, 1000);
+		target.bookWithDoubleMiles(new Booking[]{flight});
+		Assert.assertEquals(1000, target.bonusFrequentFlierMiles, 0.01);	
+	}
 	
 	@After
 	public void TearDown()
