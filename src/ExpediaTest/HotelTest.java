d2248b7c90b9a7d5214a2db6713c0f5f46ae085b
siscodr <siscodr@rose-hulman.edu>
2015-04-17 20:43:08 -0400

    Cleanup of Hotel Test
    Trying to find solution to PropertyBehavior() on Easy mock.

--- src/ExpediaTest/HotelTest.java
+++ src/ExpediaTest/HotelTest.java
@@ -11,112 +11,104 @@ import org.junit.Test;
 import Expedia.Hotel;
 import Expedia.IDatabase;
 
-public class HotelTest
-{
+public class HotelTest {
 	private Hotel targetHotel;
 	private int NightsToRentHotel = 5;
-	
+
 	@Before
-	public void TestInitialize()
-	{
+	public void TestInitialize() {
 		targetHotel = new Hotel(NightsToRentHotel);
 	}
-	
+
 	@Test
-	public void TestThatHotelHasCorrectBasePriceForOneDayStay()
-	{
+	public void TestThatHotelHasCorrectBasePriceForOneDayStay() {
 		Hotel target = new Hotel(1);
 		Assert.assertEquals(45, target.getBasePrice(), 0.01);
 	}
-	
+
 	@Test
-	public void TestThatHotelHasCorrectBasePriceForTwoDayStay()
-	{
+	public void TestThatHotelHasCorrectBasePriceForTwoDayStay() {
 		Hotel target = new Hotel(2);
 		Assert.assertEquals(90, target.getBasePrice(), 0.01);
 	}
-	
+
 	@Test
-	public void TestThatHotelHasCorrectBasePriceForTenDaysStay()
-	{
+	public void TestThatHotelHasCorrectBasePriceForTenDaysStay() {
 		Hotel target = new Hotel(10);
 		Assert.assertEquals(450, target.getBasePrice(), 0.01);
 	}
-	
-	@Test(expected=RuntimeException.class)
-	public void TestThatHotelThrowsOnBadLength()
-	{
+
+	@Test(expected = RuntimeException.class)
+	public void TestThatHotelThrowsOnBadLength() {
 		new Hotel(-5);
 	}
-	
+
 	@Test
-	public void TestHotelResultsInZeroMiles(){
+	public void TestHotelResultsInZeroMiles() {
 		Hotel target = new Hotel(10);
 		Assert.assertEquals(0, target.getMiles());
 	}
 
-	
 	@Test
-    public void TestThatHotelDoesGetRoomOccupantFromTheDatabase()
-    {
-		
+	public void TestThatHotelDoesGetRoomOccupantFromTheDatabase() {
+
 		IDatabase mockDB = EasyMock.createMock(IDatabase.class);
-        String roomOccupant = "Whale Rider";
-        String anotherRoomOccupant = "Raptor Wrangler";
-        
-        EasyMock.expect(mockDB.getRoomOccupant(24)).andReturn(roomOccupant);
-        EasyMock.expect(mockDB.getRoomOccupant(1025)).andReturn(anotherRoomOccupant);
+		String roomOccupant = "Whale Rider";
+		String anotherRoomOccupant = "Raptor Wrangler";
 
-        // stub default return value
-        EasyMock.expect(mockDB.getRoomOccupant(EasyMock.anyInt())).andStubReturn("Empty room");
-        
-        EasyMock.replay(mockDB);
-       
-        Hotel target = new Hotel(10);
-        target.Database = mockDB;
+		EasyMock.expect(mockDB.getRoomOccupant(24)).andReturn(roomOccupant);
+		EasyMock.expect(mockDB.getRoomOccupant(1025)).andReturn(
+				anotherRoomOccupant);
 
-        String result;
+		// stub default return value
+		EasyMock.expect(mockDB.getRoomOccupant(EasyMock.anyInt()))
+				.andStubReturn("Empty room");
 
+		EasyMock.replay(mockDB);
 
-        result = target.getRoomOccupant(25);
-        Assert.assertEquals("Empty room", result);
+		Hotel target = new Hotel(10);
+		target.Database = mockDB;
 
-        result = target.getRoomOccupant(1025);
-        Assert.assertEquals(anotherRoomOccupant, result);
+		String result;
 
-        result = target.getRoomOccupant(24);
-        Assert.assertEquals(roomOccupant, result);
+		result = target.getRoomOccupant(25);
+		Assert.assertEquals("Empty room", result);
 
-        result = target.getRoomOccupant(23);
-        Assert.assertEquals("Empty room", result);
+		result = target.getRoomOccupant(1025);
+		Assert.assertEquals(anotherRoomOccupant, result);
 
-        //mocks.VerifyAll();
-        EasyMock.verify(mockDB);
-    }
-//    @Test
-//    public void TestThatHotelDoesGetRoomCountFromDatabase()
-//    {
-//    	IDatabase mockDB = EasyMock.createNiceMock(IDatabase.class);
-//        List<String> Rooms = new LinkedList<String>();
-//        for (int i = 0; i < 100; i++)
-//        {
-//            Rooms.add(i, "");;
-//        }
-//
-//        // FIXME: what is the EasyMock equivalent, if any?
-//        //EasyMock.expect(mockDB.Rooms).PropertyBehavior();
-//        
-//        EasyMock.replay(mockDB);
-//
-//        mockDB.Rooms = Rooms;
-//
-//
-//        Hotel target = new Hotel(10);
-//        target.Database = mockDB;
-//
-//        int roomCount = target.AvailableRooms();
-//        Assert.assertEquals(Rooms.size(), roomCount);
-//
-//        EasyMock.verify(mockDB);
-//    }
+		result = target.getRoomOccupant(24);
+		Assert.assertEquals(roomOccupant, result);
+
+		result = target.getRoomOccupant(23);
+		Assert.assertEquals("Empty room", result);
+
+		// mocks.VerifyAll();
+		EasyMock.verify(mockDB);
+	}
+	// @Test
+	// public void TestThatHotelDoesGetRoomCountFromDatabase()
+	// {
+	// IDatabase mockDB = EasyMock.createMock(IDatabase.class);
+	// List<String> Rooms = new LinkedList<String>();
+	// for (int i = 0; i < 100; i++)
+	// {
+	// Rooms.add(i, "");;
+	// }
+	//
+	// // FIXME: what is the EasyMock equivalent, if any?
+	// //EasyMock.expect(mockDB.Rooms).PropertyBehavior();
+	// //expect
+	// EasyMock.expect(mockDB.Rooms.size()).andReturn(new Integer(100));
+	// EasyMock.replay(mockDB);
+	//
+	//
+	// Hotel target = new Hotel(10);
+	// target.Database = mockDB;
+	//
+	// int roomCount = target.AvailableRooms();
+	// Assert.assertEquals(Rooms.size(), roomCount);
+	//
+	// EasyMock.verify(mockDB);
+	// }
 }
